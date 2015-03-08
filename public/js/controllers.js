687004cf3bce8d7cad6578e2953f69661469d59f
theshteves <kneisers@msu.edu>
2015-03-07 23:26:39 -0500

    Added controllers

---
+++ public/js/controllers.js
@@ -0,0 +1,9 @@
+var soundApp = angular.module('soundApp', []);
+
+soundApp.controller('soundCtrl', function ($scope) {
+    $scope.name = '';
+});
+
+soundApp.controller('boardCtrl', function ($scope) {
+    $scope.name = '';
+});
