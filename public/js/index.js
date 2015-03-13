d0ea7f632d2425650905377873e28e52f343854e
theshteves <kneisers@msu.edu>
2015-03-12 23:14:39 -0400

    Generated buttons now have linked audio

--- public/js/index.js
+++ public/js/index.js
@@ -2,46 +2,45 @@ $(document).ready(function() {
 
     // Add sound
     $("#submit").on("click", function() {
-	var name = $("#InputSound").val(),
+
+	var name = $("#InputName").val(),
 	    key = null,
 	    color = null,
-	    file = $("#InputFile").val();
-	addButton(name, key, color, file);
+	    path = $("#InputSource").val();
+
+	// Set defaults
+
+
+	addButton(name, key, color, path);
 
 	// Reset inputs
-
+	$("#InputName").val("");
+	$("#InputSource").val("");
     });
 });
 
-function addButton(name, key, color, file) {
+function addButton(name, key, color, path) {
 
     // Null parameter exeptions
     if (key == null) {
 	key = "A";
     }
     if (color == null) {
-	color = "primary"; // See bootstrap button color ids for others
+	color = "primary";
     }
 
-    var newSpace = document.createElement("TD"),
-	newBtn = document.createElement("button"),
-	newLabel = document.createElement("H4"),
-	newIcon = document.createElement("SPAN"), // h5-6 instead?
-	labelText = document.createTextNode(name),
-	iconText = document.createTextNode(key);
-
     // Determine next available slot for button
-    var btnId = 0;
+    var idNum = 0;
     var newRow = false;
     for (var row = 1; row < 9; row += 1) {
 
-	for (var cell = 1; cell < 5; cell += 1) {
+	for (var cell = 1; cell < 9; cell += 1) {
 	    if (!document.getElementById(row + "-" + cell)) {
-		btnId = row + "-" + cell;
+		idNum = row + "-" + cell;
 	    }
 	}
 
-	if (btnId != 0) {
+	if (idNum != 0) {
 	    if (!document.getElementById("row-" + row) ) {
 		newRow = document.createElement("TR");
 		newRow.setAttribute("id", "row-" + row);
@@ -50,14 +49,33 @@ function addButton(name, key, color, file) {
 	}
     }
 
-    newBtn.setAttribute("id", btnId);
+    // Create new elements for button
+    var newSpace = document.createElement("TD"),
+	newBtn = document.createElement("button"),
+	newSoundPlayer = document.createElement("AUDIO"),
+	newSoundSource = document.createElement("SOURCE"),
+	newLabel = document.createElement("H4"),
+	newSubLabel = document.createElement("H6");
+
+    // Assemble button
+    newSoundSource.setAttribute("type", "audio/*");
+    newSoundSource.setAttribute("src", path);
+    newSoundPlayer.appendChild(newSoundSource);
+    newSoundPlayer.setAttribute("id", "sound-" + idNum);
+    newBtn.appendChild(newSoundPlayer);
+
+    var labelText = document.createTextNode(name);
+    newLabel.appendChild(labelText);
+    newBtn.appendChild(newLabel);
+
+    var subLabelText = document.createTextNode(key);
+    newSubLabel.appendChild(subLabelText);
+    newSubLabel.setAttribute("aria-hidden", "true");
+    newBtn.appendChild(newSubLabel);
+
+    newBtn.setAttribute("id", idNum);
     newBtn.setAttribute("type", "button");
     newBtn.setAttribute("class", "btn");
-    newLabel.appendChild(labelText);
-    newIcon.setAttribute("aria-hidden", "true");
-    newIcon.appendChild(iconText);
-    newBtn.appendChild(newLabel);
-    newBtn.appendChild(newIcon);
     newSpace.appendChild(newBtn);
 
     // Attach to DOM based on whether new row was generated
